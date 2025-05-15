3796abe8c3f4dc5942ddf8973e56fce1dbf105bc
hadi499 <hadi.ygcl@gmail.com>
2025-05-15 07:23:06 +0700

    first

---
+++ auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { prisma } from "@/lib/prima";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [Google],
+  session: { strategy: "jwt" },
+  pages: {
+    signIn: "/signin",
+  },
+});
